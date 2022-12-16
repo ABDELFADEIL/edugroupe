7fa108bcacf94aa830fa4cac91ddd937a9903caf
ABDELFADEIL <abdalfadeil@gmail.com>
2022-12-16 12:12:36 +0100

    gulp config

---
+++ gescom_angular/gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const war = require('gulp-war');
+const zip = require('gulp-zip');
+
+gulp.task('war', function (callback) {
+  gulp.src(['./asmar/**'])
+    .pipe(war({
+      welcome: 'index.html',
+      displayName: 'asmar',
+    }))
+    .pipe(zip('asmar.war'))
+    .pipe(gulp.dest("./"));
+  callback();
+});
